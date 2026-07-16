fc160c16678061dd365dd6e45e4762f2c4038671
Habib Ibrahim <aliyuibrahimaliyu0858@gmail.com>
2026-07-16 21:36:03 +0100

    Added DB port

--- config/db.php
+++ config/db.php
@@ -8,7 +8,7 @@ $password = getenv("MYSQLPASSWORD");
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$database;charset=utf8mb4",
+        "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4",
         $user,
         $password
     );
